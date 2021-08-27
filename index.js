153a04140605efd0b3813346dd52baf70e9fdb2d
Shane Bramble-Wade <sbramwade@msn.com>
2021-08-27 15:34:15 -0400

    included inquirer prompt

---
+++ index.js
@@ -0,0 +1,33 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+var addTeamMembers = function () {
+    console.log('Please Enter your Team Members:');
+
+    inquirer.prompt([{
+            type: 'input',
+            name: 'name',
+            message: "Please enter team manager's name:"
+        },
+        {
+            type: 'number',
+            name: 'id',
+            message: "Please enter team manager's id:"
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter manager's email address:"
+        },
+        {
+            type: 'number',
+            name: 'number',
+            message: "Please enter office number:"
+        }
+    ])
+    .then (ans => {
+        console.log(ans);
+    });
+};
+
+addTeamMembers();
