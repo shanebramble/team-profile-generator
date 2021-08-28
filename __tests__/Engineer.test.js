a88d03702214a139bf6de3cc82b6ca4b5220a299
Shane Bramble-Wade <sbramwade@msn.com>
2021-08-28 11:37:08 -0400

    finalized the class and constructor for Engineer object

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,5 +1,17 @@
 const Engineer = require('../lib/Engineer');
 
-test(`create an Engineer object`, () => {
-    const engineerEmployee = new Engineer();
+test(`Get GitHub Name from object property`, () => {
+    const e = new Engineer("Shane", 190330,"sbramwade@msn.com","randomGitUser");
+    expect(e.github).toBe("randomGitUser");
+});
+
+test("Get Engineer role from getRole()", () => {
+    
+    const e = new Engineer("Shane", 190330, "sbramwade@msn.com", "randomGitUser");
+    expect(e.getRole()).toBe("Engineer");
+});
+
+test("Get GitHub username via getGithub()", () => {
+    const e = new Engineer("Shane", 190330, "sbramwade@msn.com", "randomGitUser");
+    expect(e.getGithub()).toBe("randomGitUser");
 });
